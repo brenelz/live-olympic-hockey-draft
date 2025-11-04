82d1a9da1432e94543916126c193175ea9b81dbc
Brenley Dueck <brenleydueck@gmail.com>
2025-11-03 20:53:30 -0600

    Initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,46 @@
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/solid-router'
+import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
+import ConvexProvider from '../integrations/convex/provider.tsx'
+
+import '@fontsource/inter'
+
+import { HydrationScript } from 'solid-js/web'
+import { Suspense } from 'solid-js'
+
+import Header from '../components/Header'
+
+import styleCss from '../styles.css?url'
+
+export const Route = createRootRouteWithContext()({
+  head: () => ({
+    links: [{ rel: 'stylesheet', href: styleCss }],
+  }),
+  shellComponent: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <html>
+      <head>
+        <HydrationScript />
+      </head>
+      <body>
+        <HeadContent />
+        <Suspense>
+          <ConvexProvider>
+            <Header />
+
+            <Outlet />
+            <TanStackRouterDevtools />
+          </ConvexProvider>
+        </Suspense>
+        <Scripts />
+      </body>
+    </html>
+  )
+}

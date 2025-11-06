214b60253af6f8357acfc33006f43b63059bf011
Brenley Dueck <brenleydueck@gmail.com>
2025-11-05 21:40:07 -0600

    fix redirects

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, Outlet, redirect } from '@tanstack/solid-router'
 export const Route = createFileRoute('/_authed')({
     component: () => <Outlet />,
     beforeLoad: async (ctx) => {
-        if (!ctx.context.token) {
+        if (!ctx.context.session) {
             throw redirect({ to: "/" });
         }
     },
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { Button } from "~/components/ui/button";
 
 export const Route = createFileRoute("/")({
   beforeLoad: async (ctx) => {
-    if (ctx.context.token) {
+    if (ctx.context.session) {
       throw redirect({ to: "/dashboard" });
     }
   },

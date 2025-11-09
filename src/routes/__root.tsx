1af70a9d4300accfae9761b59fd4517adb4b1699
Brenley Dueck <brenleydueck@gmail.com>
2025-11-09 11:36:40 -0600

    add lang attribute

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ export const Route = createRootRouteWithContext()({
 
 function RootComponent() {
   return (
-    <html>
+    <html lang="en">
       <head>
         <HydrationScript />
       </head>
